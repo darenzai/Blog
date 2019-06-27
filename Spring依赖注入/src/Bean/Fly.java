fddb2f9298b90478f6187399c7f9fd4d6441200b
darenzai <1453566224@qq.com>
2019-06-27 08:55:30 +0800

    two

--- Spring依赖注入/src/Bean/Fly.java
+++ Spring依赖注入/src/Bean/Fly.java
@@ -2,6 +2,14 @@ package Bean;
 
 public class Fly {
 
+    private  bird bird;
+    private String name;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- Spring依赖注入/src/Bean/bird.java
+++ Spring依赖注入/src/Bean/bird.java
@@ -5,6 +5,11 @@ public class bird {
     private String name;
 
 
+    public String getName() {
+        return name;
+    }
 
-
+    public void setName(String name) {
+        this.name = name;
+    }
 }
